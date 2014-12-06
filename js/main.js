643002a28314583b37ae67bb4c24019915eea1ca
Jacob Singh <jacob@macbookair-jacobsingh.home>
2014-12-06 16:44:17 +0530

    wooot. Basic version working

--- js/main.js
+++ js/main.js
@@ -1,50 +1,125 @@
+DEBUG = false;
+//DEBUG_FROM = "mix";
 (function($) {
     $(document).ready(function(){
 
-    var makeFace = function() {
-        $(this).faceDetection({
-            complete: function (faces) {
-                console.log(faces);
-                if (faces.length < 1) {
-                    console.log('aborting... no detection');
-                    return;
+        PS = {};
+        PS.faces = [];
+
+        //$('#mix').hide();
+        // Make this dynamic and detected.
+        KLUDGE_DEST_HEIGHT = 213;
+
+        DEBUG == true && $('#picture').attr('src', 'img/demo.jpg');
+
+        var makeFace = function() {
+            $(this).faceDetection({
+                complete: function (faces) {
+                    highLight(this, faces);
+                    var vData;
+                    if (faces.length < 1) {
+                        console.log('aborting... no detection');
+                        return;
+                    }
+
+                    vData = cropFace(this[0], faces[0]);
                 }
-                cropFace(this[0], faces[0]);
-                console.log(faces);
+            });
+        };
+
+        function highLight(elem, faces) {
+            for (var i = 0; i < faces.length; i++) {
+
+                $('<div>', {
+                    'class':'face',
+                    'css': {
+                        'position': 'absolute',
+                        'left':     faces[i].x * faces[i].scaleX + 'px',
+                        'top':      faces[i].y * faces[i].scaleY + 'px',
+                        'width':    faces[i].width  * faces[i].scaleX + 'px',
+                        'height':   faces[i].height * faces[i].scaleY + 'px'
+                    }
+                }).insertAfter(elem);
             }
-        });
-    };
+        }
 
-    function takeSnapshot() {
-        Webcam.snap( function(data_uri) {
-            console.log(data_uri.length);
-            $('#picture').attr('src', data_uri);
-        } );
-    };
+        function takeSnapshot() {
+            Webcam.snap( function(data_uri) {
+                console.log(data_uri.length);
+                $('#picture').attr('src', data_uri);
+            } );
+        };
+
+        var addFaceToFaces = function() {
+            var img, myFace;
+            myFace = $('<img />');
+            myFace.attr('src', $('canvas', this)[0].toDataURL());
+            $('#faces').append(myFace);
+        }
+
+        function cropFace(img, cropData) {
+            var ctx, canvas;
+            canvas = $('#face')[0];
+            ctx = canvas.getContext('2d');
+            ctx.drawImage(img, cropData.x, cropData.y, cropData.width, cropData.height, 0, 0,  canvas.height,canvas.width);
+        }
+
+        function makeMix() {
+            var faces;
+            var pickedFaces = [];
+            var rand;
+            var i,j;
+            var pieces = 3;
+            var srcSectionHeight, dstSectionHeight;
+            var sourceY = 0;
+            var destY = 0;
+            var img, imgWidth, canvas, ctx ;
 
 
-    function cropFace(img, cropData) {
-        var tempCtx, tempCanvas, destWidth;
-        tempCanvas = document.createElement('canvas');
-        tempCtx = tempCanvas.getContext('2d');
-        //tempCanvas.width = theSelection.w;
-        //tempCanvas.height = theSelection.h;
-        destWidth = cropData.width *  tempCanvas.height /cropData.height
-        tempCtx.drawImage(img, cropData.x, cropData.y, cropData.width, cropData.height, 0, 0, destWidth, tempCanvas.height);
-        var vData = tempCanvas.toDataURL();
-        $('#face').attr('src', vData);
-    }
+            canvas = document.getElementById('result');
+            ctx = canvas.getContext('2d');
+            dstSectionHeight = canvas.height / pieces;
 
-    $('#picture').click(makeFace);
-    $('#my-camera').click(takeSnapshot);
+            // pick 3 random ones;
+            var faces = $('#faces img');
+            faces.sort(function(a,b) { return Math.random() > 0.5 ? -1 : 1});
+            pickedFaces = faces.slice(0,pieces);
+
+            srcSectionHeight = pickedFaces[0].naturalHeight / pieces;
+            imgWidth = pickedFaces[0].naturalWidth;
 
 
+            for (j=0; j<pickedFaces.length; j++) {
+                img = pickedFaces[j];
+                console.log(j);
+                ctx.drawImage(img, 0, sourceY, imgWidth, srcSectionHeight, 0, destY, canvas.width, dstSectionHeight);
+                sourceY += srcSectionHeight;
+                destY += dstSectionHeight;
+            }
+            console.log(pickedFaces);
+        }
+
+        //function getSection(img, )
+
+        $('#picture').click(makeFace);
+        $('#my-camera').click(takeSnapshot);
+        $('#face-wrapper').click(addFaceToFaces);
+        $('#mix').click(makeMix);
+
+        if (DEBUG === true && DEBUG_FROM === "mix") {
+            myFace = $('<img />');
+            myFace.attr('src', 'img/face1.jpg');
+            $('#faces').append(myFace);
+            myFace = $('<img />');
+            myFace.attr('src', 'img/face2.jpg');
+            $('#faces').append(myFace);
+            myFace = $('<img />');
+            myFace.attr('src', 'img/face3.jpg');
+            $('#faces').append(myFace);
+        }
     });
 
 })(jQuery);
 
-Webcam.attach( '#my-camera' );
 
-Webcam.on('load', function() {
-
-});
+DEBUG === false && Webcam.attach( '#my-camera' );
